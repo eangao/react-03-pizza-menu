ce55b594be881b014904bf92af3a4dee777f78a9
elmar <elmarcera@gmail.com>
2024-01-29 14:02:09 +0800

    Rendering the Root Component and Strict Mode

---
+++ src/index.js
@@ -0,0 +1,29 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+function App() {
+  return <h1>Hello React!</h1>;
+}
+
+// React v18
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// And so the app is now wrapped inside of this strict mode.
+// And strict mode is really not a big deal.
+// The only thing that it does is that during development
+// it will render all components twice
+// in order to find certain bugs.
+// And also React will check
+// if we're using outdated parts of the React API.
+// So strict mode is nothing groundbreaking
+// but it's still a good idea to always activate it
+// when we develop React applications.
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// React before 18
+// React.render(<App />)
